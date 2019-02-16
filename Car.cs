ef8f8663914be64d2f35accd39f5e0671cc6414c
Ербол Иргалиев <rasherow222@gmail.com>
2019-02-16 22:58:23 +0600

    .gitignore fix

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_07_02
+{
+    public class Car
+    {
+        public double Weight { get; set; }
+        public double MaxSpeed { get; set; }
+        public int PlacesCnt { get; set; }
+        public string Model { get; set; }
+    }
+}
